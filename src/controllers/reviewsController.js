64cff5afbcbb2cdc981a7b8f5f3cca5c23e20964
Alexandre Morozoff <morozoff_alexandre@outlook.com>
2025-09-23 16:08:02 +0200

    Reviews get work

--- src/controllers/reviewsController.js
+++ src/controllers/reviewsController.js
@@ -1,5 +1,20 @@
-// TODO: lister les critiques d'un film
+import { listReviewsByFilmId } from "../services/reviewsServices.js";
 
+// TODO: lister les critiques d'un film
+export async function listReviews(req, res, next) {
+  try {
+    const id = Number(req.params.id);
+    if (!Number.isInteger(id) || id <= 0) {
+      const err = new Error("Paramètre 'id' invalide");
+      err.status = 400;
+      throw err;
+    }
+    const reviewsList = await listReviewsByFilmId(id);
+    res.status(200).json(reviewsList);
+  } catch (error) {
+    next(error);
+  }
+}
 // TODO: créer une critique liée à un film
 
 // TODO: supprimer une critique
--- src/services/reviewsServices.js
+++ src/services/reviewsServices.js
@@ -1,6 +1,29 @@
+import { query } from "../db.js";
+
 export async function listReviewsByFilmId(
   filmId,
   { limit = 50, offset = 0 } = {}
-) {}
-export async function createReview(filmId, { author, rating, comment }) {}
-export async function deleteReview(id) {}
+) {
+  if (!Number.isInteger(filmId) || filmId <= 0) {
+    const err = new Error("filmId invalide");
+    err.status = 400;
+    throw err;
+  }
+
+  const sql = `SELECT *
+  FROM reviews
+  WHERE film_id = $1
+  ORDER BY created_at DESC
+  LIMIT $2 OFFSET $3
+  `;
+  const { rows } = await query(sql, [filmId, limit, offset]);
+  if (rows.length === 0) {
+    const err = new Error("Aucune reviews n'a été trouvé");
+    err.status = 404;
+    throw err;
+  }
+  return rows;
+}
+
+// export async function createReview(filmId, { author, rating, comment }) {}
+// export async function deleteReview(id) {}
